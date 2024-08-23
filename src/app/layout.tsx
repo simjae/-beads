75fb6a8a63fdc7387d3bfb63a8471c2a9a640b23
adrock_sim <adrock_sim@adrock-simui-MacBookPro.local>
2024-08-23 10:15:44 +0900

    feat: 🎸 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,27 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../../src/styles/globals.css";
 import MetaTags from "@src/components/Seo/MetaTags";
+import { DM_Sans } from "next/font/google";
+import { Space_Mono } from "next/font/google";
+
 const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
 //   title: "픽셀 아트",
 //   description: "픽셀 아트를 만들어보세요",
 // };
+
+const fontHeading = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
+});
+const fontBody = Space_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+  weight: "400",
+});
 
 export default function RootLayout({
   children,
